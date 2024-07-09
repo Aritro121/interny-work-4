78d97a80c7c058390b894c0830c3df2896bf7193
Aritro121 <113583330+Aritro121@users.noreply.github.com>
2024-07-09 20:45:43 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<?php
+    $a = 18; $b = 15; $c = 20;
+
+    if($a<$b){
+        echo "The smalest number is ".$a;
+    }
+    elseif($b<$c){
+        echo "The smalest number is ".$b ;
+    }
+    else {
+        echo "The smalest number is ".$c;
+    }
+?>
